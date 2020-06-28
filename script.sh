64c27f5873f3d7fcc9ca175a90da9cc337550a4c
Harsh Singhvi <harshsinghvi98@gmail.com>
2020-06-28 17:45:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,4 @@
-docker run -p 80:80 -p 443:443 -v $PWD/src:/var/www/html/ insomniaccoder/phishing:instagram
+# sudo docker stop phishing-tg 
+# sudo docker rm phishing-tg 
+# docker run --name phishing-tg -d -p 80:80 -p 443:443 -v $PWD/src:/var/www/html/ insomniaccoder/phishing:instagram
+echo "Hello World !!  "
